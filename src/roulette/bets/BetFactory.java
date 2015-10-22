312113c71454df6b187fd5b10fba18eaa7ea6d82
Mike Ma <ym67@duke.edu>
2015-10-22 17:01:18 -0400

    Refactor Step 2

--- src/roulette/bets/BetFactory.java
+++ src/roulette/bets/BetFactory.java
@@ -1,41 +1,46 @@
 package roulette.bets;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import roulette.Bet;
 
 public class BetFactory {
-	List<String> bets = new ArrayList<String>();
-	
+	Map<String, Integer> oddMap;
+	Map<String, String> descriptionMap;
+	String[] bets = "OddEven RedBlack ThreeConsecutive".split(" ");
+
 	public BetFactory() {
-		bets.add(0, "OddEven");
-		bets.add(1, "RedBlack");
-		bets.add(2, "ThreeConsecutive");
+		oddMap = new HashMap<>();
+		descriptionMap = new HashMap<>();
+		oddMap.put("OddEven", 1);
+		oddMap.put("RedBlack", 1);
+		oddMap.put("ThreeConsecutive", 11);
+		descriptionMap.put("RedBlack", "Red Black Bet");
+		descriptionMap.put("OddEven", "Odd Even Bet");
+		descriptionMap.put("ThreeConsecutive", "3 Consecutive Bet");
 	}
 
 	public Bet getBet(int betType) {
-		switch (betType) {
-		case 1:
-			return new OddEven("Odd even", 1);
-		case 2:
-			return new RedBlack("Red black", 1);
-		case 3:
-			return new ThreeConsecutive("TreeConsecutive", 11);
-		default:
+		try {
+			String name = this.getClass().getPackage().getName() + "." + bets[betType-1];
+			Class[] types = { String.class, Integer.TYPE };
+			Constructor constructor = Class.forName(name).getDeclaredConstructor(types);
+			return (Bet) constructor.newInstance(descriptionMap.get(bets[betType-1]), oddMap.get(bets[betType-1]));
+		} catch (Exception e) {
+			e.printStackTrace();
 			return null;
 		}
 	}
 
-	public List<String> getPossibleBets() {
-		return Collections.unmodifiableList(bets);
-	}
-
 	public void printAllBets() {
 		System.out.println("You can make one of the following types of bets:");
-		for (int k = 0; k < bets.size(); k++) {
-			System.out.println(String.format("%d) %s", (k + 1), bets.get(k)));
+		for (int k = 0; k < bets.length; k++) {
+			System.out.println(String.format("%d) %s", (k + 1), bets[k]));
 		}
 	}
 }
